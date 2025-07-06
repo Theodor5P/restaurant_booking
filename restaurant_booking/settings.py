ae4804f9009a7f7bfb7789d1af35645ff9ded563
Theodor5P <wwtlx9hw@students.codeinstitute.net>
2025-07-06 23:31:43 +0200

    add SECRET_KEY

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vdd*@kln#aw&*o9(s&2x@f57*7$r36kg#bx^1@w!)dj=z1-^64'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
